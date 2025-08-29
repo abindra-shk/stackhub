31af9ee503e43ea57304a563724ab7043a0a6b90
Abindra <abindrashakya.123@gmail.com>
2025-08-29 08:30:27 +0545

    mongoose set up

--- backend-express/server.js
+++ backend-express/server.js
@@ -1,7 +1,12 @@
-// server.js
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./src/config/db");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware
 app.use(express.json());
@@ -11,5 +16,5 @@ const routes = require("./src/routes");
 app.use("/api", routes);
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}/api`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
--- backend-express/src/routes/index.js
+++ backend-express/src/routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const userRoutes = require("./userRoutes");
+
 router.get("/", (req, res) => {
   res.send({ message: "Welcome to Stackhub Express Backend 🚀" });
 });
+
+router.use("/users", userRoutes);
 
 module.exports = router;
